4ef02647e952873bdccf0c542722603b023860e8
Tomas Birbe <tomas.birbe@gmail.com>
2022-02-20 22:43:45 -0300

    Fix: Test in order to deploy to vercel

--- index.ts
+++ index.ts
@@ -5,9 +5,9 @@ import entryRouter from './src/api/entry';
 import { testRouter } from './src/api/test';
 const app = express();
 
-connection.sync();
+// connection.sync();
 app.use(json());
-app.use('/api/entry', entryRouter);
+// app.use('/api/entry', entryRouter);
 app.use('/api/test', testRouter);
 const server = app.listen(3001, () => console.log('Listen!'));
 
