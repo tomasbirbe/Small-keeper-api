56d3f531570cfa746154457b1d7624a2a4d119ca
Tomas Birbe <tomas.birbe@gmail.com>
2022-02-20 22:37:57 -0300

    Fix: Test in order to deploy to heroku

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,9 +5,9 @@ import entryRouter from '../api/entry';
 import { testRouter } from '../api/test';
 const app = express();
 
-connection.sync();
+// connection.sync();
 app.use(json());
-app.use('/api/entry', entryRouter);
+// app.use('/api/entry', entryRouter);
 app.use('/api/test', testRouter);
 const server = app.listen(3001, () => console.log('Listen!'));
 
