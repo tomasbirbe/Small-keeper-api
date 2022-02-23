d6e5005d2e719b989c8beaeedc1dbc0a9b8d5e2a
Tomas Birbe <tomas.birbe@gmail.com>
2022-02-23 14:05:18 -0300

    TestForDeploy: Compile ts files to js files in order to deploy on heroku

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'import'],
-  ignorePatterns: ['*.json'],
+  ignorePatterns: ['*.json', '/dist/*'],
   rules: {
     'prettier/prettier': [
       'error',
